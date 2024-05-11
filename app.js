bcde9f8e69afc68484ac07ee32b05f6e6583631c
Rafa <108197293+Rafacampos16@users.noreply.github.com>
2024-05-11 13:27:17 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,104 @@
+/* let x = 10
+console.log(x)
+x++
+console.log(x)
+++x
+console.log(x)
+x--
+console.log(x)
+--x
+console.log(x) */
+
+//-------------------------//
+
+/*let x = 10
+console.log(x++)
+console.log(x)*/
+
+//-------------------------//
+
+/*let x = 10
+console.log(x--)
+console.log(x)
+*/
+
+
+//-------------------------//
+
+/*let x =10
+console.log(x)
+let aux = 5
+x += aux
+console.log(x)
+
+
+x-=5
+console.log(x)
+
+x*=2
+console.log(x)
+
+x/=2
+console.log(x)
+
+x%=3
+console.log(x)*/
+
+//-------------------------//
+
+/*()
+//variáveis
+let pi = 3.14
+console.log(pi)
+
+pi=3.1415
+console.log(pi)
+
+//constantes
+const PI =3.1415
+console.log(PI)
+
+console.log(Math.PI)
+let pi2 = Math.PI
+console.log(pi2)
+console.log(Math.PI * 2)
+
+console.log(Math.pow(7,2));
+let n1 = 33
+let n2 = (Math.pow(n1, 3))
+console.log(n2)
+
+console.log(Math.sqrt(49))
+let n3 = 55.55
+console.log(Math.sqrt(n3))
+
+console.log(Math.cbrt(27))
+let n4 = 81
+console.log(Math.cbrt(n4))
+*/
+
+//-------------------------//
+
+
+let n5 = 3.333
+console.log(n5)
+console.log(Math.abs(n5))
+
+console.log(Math.ceil(n5))
+
+console.log(Math.floor(n5))
+console.log(Math.round(n5))
+n5= 3.99
+console.log(Math.round(n5))
+
+console.log(Math.max(5,7,-5,8,11))
+
+console.log(Math.min(5,7,-5,8,11))
+
+console.log(Math.round(Math.random()*10+1))
+
+
+
+
+
+
